f7dd20934fd6ca3f3f12a32e36dbeb38659a898b
Iza Lopes <102091381+Izalp@users.noreply.github.com>
2024-06-07 21:28:21 -0300

    add script.js ✨

--- dashboard/script.js
+++ dashboard/script.js
@@ -2,10 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuIcon = document.getElementById('menu');
     const profileIcon = document.getElementById('perfil');
     const searchInput = document.getElementById('txtBusca');
+    const eventosInput = document.getElementById('txtBusca'); 
     const infoButtons = document.querySelectorAll('.mais_informacoes');
+    const inscricaoButton = document.querySelectorAll('.inscricao');
+    const indisponivelButton = document.querySelectorAll('.indisponivel');
     const courseCards = document.querySelectorAll('.cartao_disciplina');
+    const eventsCards = document.querySelectorAll('.cartao_eventos');
     const sidebar = document.getElementById('sidebar');
     const profileSidebar = document.getElementById('profile-sidebar');
+    const eventosInatel = document.getElementById('eventos-inatel'); 
+    const servicosacademicos = document.getElementById('servicos-academicos'); 
+
 
     menuIcon.addEventListener('click', (event) => {
         event.stopPropagation();
@@ -38,11 +45,45 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+    eventosInput.addEventListener('input', () => {   
+        const searchText = eventosInput.value.trim().toLowerCase();
+        eventsCards.forEach(card => {
+            const courseTitle = card.querySelector('h2').innerText.trim().toLowerCase();
+            if (courseTitle.includes(searchText)) {
+                card.style.display = '';
+            } else {
+                card.style.display = 'none';
+            }
+        });
+    });
+
     infoButtons.forEach(button => {
         button.addEventListener('click', (event) => {
             const courseCard = event.target.closest('.cartao_disciplina');
             const infoCard = courseCard.querySelector('.cartao_informacoes');
             infoCard.style.display = (infoCard.style.display === 'block') ? 'none' : 'block';
         });
+    });
+
+    
+    inscricaoButton.forEach(button => {
+        button.addEventListener('click', (event) => {
+            infoCard.style.display = (infoCard.style.display === 'block') ? 'none' : 'block';
+        });
+    });
+
+    
+    indisponivelButton.forEach(button => {
+        button.addEventListener('click', (event) => {
+            infoCard.style.display = (infoCard.style.display === 'block') ? 'none' : 'block';
+        });
+    });
+
+    eventosInatel.addEventListener('click', () => {
+        window.location.href = 'telaFuncionalidade.html'; 
+    });
+
+    servicosacademicos.addEventListener('click', () => {
+        window.location.href = 'telaMenu.html';
     });
 });
